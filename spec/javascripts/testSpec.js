9c124b77bf555890c6861e5f0569bd0bdc109216
Oscar <oscarbc@me.com>
2014-08-07 16:03:09 +0200

    RED

---
+++ spec/javascripts/testSpec.js
@@ -0,0 +1,24 @@
+// TO DO LIST
+
+/*
+	1.   => 
+	2.  hola => hola
+	3.  hola adeu => hola adeu
+	4.  carlos@iexpertos.com => carlos@iexpertos.com
+	5.  carlos(AT)iexpertos.com => carlos@iexpertos.com
+	6.  carlos(AT)iexpertos(DOT)com => carlos@iexpertos.com 
+	7.  I want to send an email to carlos(AT)iexpertos.com => I want to send an email to carlos@iexpertos.com
+	8.  I want to send an email to carlos(AT)iexpertos(DOT)com => I want to send an email to carlos@iexpertos.com
+	9.  I want to send an email to carlos-AT-iexpertos-DOT-com => I want to send an email to carlos@iexpertos.com
+	10. I want to send an email to carlos(AT)iexpertos-DOT-com => I want to send an email to carlos@iexpertos.com
+	11. I want to send an email to carlos-AT-iexpertos(DOT)com => I want to send an email to carlos@iexpertos.com
+*/
+function converter (arg) {
+
+}
+
+describe("Hide email converter", function() {
+	it("returns an empty string if input is empty", function() {
+		expect(converter("")).toBe("");
+	});
+});
